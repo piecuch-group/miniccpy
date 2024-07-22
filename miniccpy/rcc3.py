61eea5a9aa469ea3520104eba2ca5be34d88fd73
Karthik Gururangan <gururang@msu.edu>
2024-07-22 19:48:11 -0400

    Optimized RHF-CC3 ground-state code to remove usage of full T3 array.

--- miniccpy/rcc3.py
+++ miniccpy/rcc3.py
@@ -5,24 +5,10 @@ from miniccpy.helper_cc import get_rccs_intermediates
 from miniccpy.diis import DIIS
 from miniccpy.utilities import get_memory_usage
 
-def singles_residual(t1, t2, t3, f, g, o, v):
+def singles_residual(t1, t2, f, g, o, v):
     """Compute the projection of the CCSDT Hamiltonian on singles
         X[a, i] = < ia | (H_N exp(T1+T2+T3))_C | 0 >
     """
-    # symmetric quantities
-    #t3s = t3 - t3.transpose(0, 1, 2, 3, 5, 4) + t3.transpose(0, 1, 2, 4, 5, 3) - t3.transpose(0, 1, 2, 4, 3, 5) + t3.transpose(0, 1, 2, 5, 3, 4) - t3.transpose(0, 1, 2, 5, 4, 3)
-    #t3b = t3 - t3.transpose(0, 1, 2, 4, 3, 5)
-    #t3c = t3 - t3.transpose(0, 1, 2, 3, 5, 4)
-    # spin-summed t3: t3(ABcIJk) = 2*t3(AbcIjk) - t3(Abc
-    #                            = 2*(2*t3(abcijk) - t3(abcjik) - t3(abckji)) - 
-    t3_ss = (
-                4.0 * t3 
-                - 2.0 * t3.transpose(0, 1, 2, 4, 3, 5) 
-                - 2.0 * t3.transpose(0, 1, 2, 5, 4, 3) 
-                - 2.0 * t3.transpose(0, 1, 2, 3, 5, 4) 
-                + t3.transpose(0, 1, 2, 4, 5, 3) 
-                + t3.transpose(0, 1, 2, 5, 3, 4)
-    )
     # intermediates
     I_ov = (
              f[o, v]
@@ -54,21 +40,12 @@ def singles_residual(t1, t2, t3, f, g, o, v):
     singles_res += 2.0 * np.einsum("anef,efin->ai", I_vovv, t2, optimize=True)
     singles_res -= np.einsum("anfe,efin->ai", I_vovv, t2, optimize=True)
     singles_res += f[v, o]
-    # T3 parts
-    singles_res += 0.5 * np.einsum("mnef,aefimn->ai", g[o, o, v, v], t3_ss, optimize=True)
     return singles_res
 
-
-def doubles_residual(t1, t2, t3, f, g, o, v):
+def doubles_residual(t1, t2, f, g, o, v):
     """Compute the projection of the CCSDT Hamiltonian on doubles
         X[a, b, i, j] = < ijab | (H_N exp(T1+T2+T3))_C | 0 >
     """
-    # partially spin-summed t3(AbcIjk) = 2*t3(abcijk) - t3(abcjik) - t3(abckji)
-    t3_s = (
-            2.0 * t3
-            - t3.transpose(0, 1, 2, 4, 3, 5)
-            - t3.transpose(0, 1, 2, 5, 4, 3)
-    )
     # intermediates
     H1, H2 = get_rccs_intermediates(t1, f, g, o, v)
     I_vv = (
@@ -99,10 +76,6 @@ def doubles_residual(t1, t2, t3, f, g, o, v):
     doubles_res += 0.5 * np.einsum("mnij,abmn->abij", I_oooo, t2, optimize=True)
     doubles_res += 0.5 * np.einsum("abef,efij->abij", g[v, v, v, v], tau, optimize=True)
     doubles_res += 0.5 * g[v, v, o, o]
-    # T3 parts
-    doubles_res += 0.5 * np.einsum("me,eabmij->abij", H1[o, v], t3_s, optimize=True)
-    doubles_res += np.einsum("amef,febmij->abij", g[v, o, v, v] + H2[v, o, v, v], t3_s, optimize=True)
-    doubles_res -= np.einsum("nmje,eabmin->abij", g[o, o, o, v] + H2[o, o, o, v], t3_s, optimize=True)
     doubles_res += doubles_res.transpose(1, 0, 3, 2)
     # remaining terms
     # can be made into (ij)(ab) pairs
@@ -152,6 +125,60 @@ def compute_ccs_intermediates(f, g, t1, t2, o, v):
     )
     return I_vooo, I_vvov
 
+def add_t3_contributions(t1, t2, f, g, I_vooo, I_vvov, e_abc, o, v):
+    nu, no = t1.shape
+    # Intermediates
+    h_ov = f[o, v] + (
+              np.einsum("mnef,fn->me", g[o, o, v, v], t1, optimize=True)
+            - np.einsum("mnfe,fn->me", g[o, o, v, v], t1, optimize=True)
+            + np.einsum("mnef,fn->me", g[o, o, v, v], t1, optimize=True)
+    )
+    h_ooov = g[o, o, o, v] + np.einsum("mnfe,fi->mnie", g[o, o, v, v], t1, optimize=True)
+    h_vovv = g[v, o, v, v] - np.einsum("nmef,an->amef", g[o, o, v, v], t1, optimize=True)
+    # RHF-adapted integral element
+    gs_oovv = 2.0 * g[o, o, v, v] - g[o, o, v, v].swapaxes(2, 3)
+    # residual containers
+    singles_res = np.zeros((nu, no))
+    doubles_res = np.zeros((nu, nu, no, no))
+    for i in range(no):
+        for j in range(no):
+            for k in range(no):
+                if i == j and j == k: continue
+                denom_occ = f[o, o][i, i] + f[o, o][j, j] + f[o, o][k, k]
+                # -h2(amij) * t2(bcmk)
+                m3 = -np.einsum("am,bcm->abc", I_vooo[:, :, i, j], t2[:, :, :, k], optimize=True) # (1)
+                m3 -= np.einsum("bm,acm->abc", I_vooo[:, :, j, i], t2[:, :, :, k], optimize=True) # (ij)(ab)
+                m3 -= np.einsum("cm,bam->abc", I_vooo[:, :, k, j], t2[:, :, :, i], optimize=True) # (ac)(ik)
+                m3 -= np.einsum("am,cbm->abc", I_vooo[:, :, i, k], t2[:, :, :, j], optimize=True) # (bc)(jk)
+                m3 -= np.einsum("bm,cam->abc", I_vooo[:, :, j, k], t2[:, :, :, i], optimize=True) # (ij)(ab)(ac)(ik)
+                m3 -= np.einsum("cm,abm->abc", I_vooo[:, :, k, i], t2[:, :, :, j], optimize=True) # (ij)(ab)(bc)(jk)
+                # h2(abie) * t2(bcek)
+                m3 += np.einsum("abe,ec->abc", I_vvov[:, :, i, :], t2[:, :, j, k], optimize=True) # (1)
+                m3 += np.einsum("bae,ec->abc", I_vvov[:, :, j, :], t2[:, :, i, k], optimize=True) # (ij)(ab)
+                m3 += np.einsum("cbe,ea->abc", I_vvov[:, :, k, :], t2[:, :, j, i], optimize=True) # (ac)(ik)
+                m3 += np.einsum("ace,eb->abc", I_vvov[:, :, i, :], t2[:, :, k, j], optimize=True) # (bc)(jk)
+                m3 += np.einsum("bce,ea->abc", I_vvov[:, :, j, :], t2[:, :, k, i], optimize=True) # (ij)(ab)(ac)(ik)
+                m3 += np.einsum("cae,eb->abc", I_vvov[:, :, k, :], t2[:, :, i, j], optimize=True) # (ij)(ab)(bc)(jk)
+                # divide by MP denominator
+                m3 /= (e_abc + denom_occ)
+                # zero out diagonal elements
+                for a in range(nu):
+                    m3[a, a, a] *= 0.0
+                # update singles residual
+                singles_res[:, i] += np.einsum('abc,bc->a', m3 - m3.swapaxes(0, 2), gs_oovv[j, k, :, :], optimize=True)
+                # symmetrize
+                m3 = (2.0 * m3
+                      - m3.swapaxes(1, 2)
+                      - m3.swapaxes(0, 2)
+                )
+                # update doubles residual
+                doubles_res[:, :, i, j] += 0.5 * np.einsum('abc,c->ab', m3, h_ov[k, :])
+                doubles_res[:, :, i, j] += np.einsum('abc,dbc->ad', m3, h_vovv[:, k, :, :])
+                doubles_res[:, :, i, :] -= np.einsum('abc,lc->abl', m3, h_ooov[j, k, :, :])
+    # Apply (ij)(ab) symmetrizer
+    doubles_res += doubles_res.transpose(1, 0, 3, 2)
+    return singles_res, doubles_res
+
 def compute_t3(I_vooo, I_vvov, t2, e_abcijk, fock, e_abc):
     """Compute the projection of the CCSDT Hamiltonian on triples
         X[a, b, c, i, j, k] = < ijkabc | (H_N exp(T1+T2+T3))_C | 0 >
@@ -172,6 +199,31 @@ def compute_t3(I_vooo, I_vvov, t2, e_abcijk, fock, e_abc):
         triples_res[:, :, :, i, i, i] *= 0.0
     for a in range(nu):
         triples_res[a, a, a, :, :, :] *= 0.0
+
+    # error = 0.0
+    # for i in range(no):
+    #     for j in range(i, no):
+    #         for k in range(j, no):
+    #             if i == j and j == k: continue
+    #             # -h2(amij) * t2(bcmk)
+    #             m3 = -np.einsum("am,bcm->abc", I_vooo[:, :, i, j], t2[:, :, :, k], optimize=True) # (1)
+    #             m3 -= np.einsum("bm,acm->abc", I_vooo[:, :, j, i], t2[:, :, :, k], optimize=True) # (ij)(ab)
+    #             m3 -= np.einsum("cm,bam->abc", I_vooo[:, :, k, j], t2[:, :, :, i], optimize=True) # (ac)(ik)
+    #             m3 -= np.einsum("am,cbm->abc", I_vooo[:, :, i, k], t2[:, :, :, j], optimize=True) # (bc)(jk)
+    #             m3 -= np.einsum("bm,cam->abc", I_vooo[:, :, j, k], t2[:, :, :, i], optimize=True) # (ij)(ab)(ac)(ik)
+    #             m3 -= np.einsum("cm,abm->abc", I_vooo[:, :, k, i], t2[:, :, :, j], optimize=True) # (ij)(ab)(bc)(jk)
+    #             # h2(abie) * t2(bcek)
+    #             m3 += np.einsum("abe,ec->abc", I_vvov[:, :, i, :], t2[:, :, j, k], optimize=True) # (1)
+    #             m3 += np.einsum("bae,ec->abc", I_vvov[:, :, j, :], t2[:, :, i, k], optimize=True) # (ij)(ab)
+    #             m3 += np.einsum("cbe,ea->abc", I_vvov[:, :, k, :], t2[:, :, j, i], optimize=True) # (ac)(ik)
+    #             m3 += np.einsum("ace,eb->abc", I_vvov[:, :, i, :], t2[:, :, k, j], optimize=True) # (bc)(jk)
+    #             m3 += np.einsum("bce,ea->abc", I_vvov[:, :, j, :], t2[:, :, k, i], optimize=True) # (ij)(ab)(ac)(ik)
+    #             m3 += np.einsum("cae,eb->abc", I_vvov[:, :, k, :], t2[:, :, i, j], optimize=True) # (ij)(ab)(bc)(jk)
+    #             # zero out diagonal elements
+    #             for a in range(nu):
+    #                 m3[a, a, a] *= 0.0
+    #             error += np.linalg.norm(m3.flatten() - triples_res[:, :, :, i, j, k].flatten())
+    # print(error)
     return triples_res * e_abcijk
 
 def kernel(fock, g, o, v, maxit, convergence, energy_shift, diis_size, n_start_diis, out_of_core, use_quasi):
@@ -180,8 +232,6 @@ def kernel(fock, g, o, v, maxit, convergence, energy_shift, diis_size, n_start_d
 
     eps = np.diagonal(fock)
     n = np.newaxis
-    e_abcijk = 1.0 / (- eps[v, n, n, n, n, n] - eps[n, v, n, n, n, n] - eps[n, n, v, n, n, n]
-                    + eps[n, n, n, o, n, n] + eps[n, n, n, n, o, n] + eps[n, n, n, n, n, o] + energy_shift )
     e_abc = -eps[v, n, n] - eps[n, v, n] - eps[n, n, v]
     e_abij = 1.0 / (-eps[v, n, n, n] - eps[n, v, n, n] + eps[n, n, o, n] + eps[n, n, n, o] + energy_shift )
     e_ai = 1.0 / (-eps[v, n] + eps[n, o] + energy_shift )
@@ -207,9 +257,9 @@ def kernel(fock, g, o, v, maxit, convergence, energy_shift, diis_size, n_start_d
 
         # Compute T3 using the perturbative approximation of CC3
         I_vooo, I_vvov = compute_ccs_intermediates(fock, g, t1, t2, o, v)
-        t3 = compute_t3(I_vooo, I_vvov, t2, e_abcijk, fock, e_abc)
-        residual_singles = singles_residual(t1, t2, t3, fock, g, o, v)
-        residual_doubles = doubles_residual(t1, t2, t3, fock, g, o, v)
+        residual_singles, residual_doubles = add_t3_contributions(t1, t2, fock, g, I_vooo, I_vvov, e_abc, o, v)
+        residual_singles += singles_residual(t1, t2, fock, g, o, v)
+        residual_doubles += doubles_residual(t1, t2, fock, g, o, v)
 
         res_norm = np.linalg.norm(residual_singles) + np.linalg.norm(residual_doubles)
 
